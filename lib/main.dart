c76692647a89e2031672ce3b646f50395fc2da8d
RGiovannini <79931478+RGiovannini@users.noreply.github.com>
2021-03-02 14:46:22 -0300

    Start project

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:garrafa_agua/app/app_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+void main() => runApp(ModularApp(module: AppModule()));
